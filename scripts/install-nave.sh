16fc36cab7f1b859a40de4474aa6eecf22b58f1b
Ron Waldon <jokeyrhyme@gmail.com>
2013-07-28 22:27:07 +1000

    remove nave install script as we use git submodule now

--- scripts/install-nave.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ ! -d ~/.nave ]; then
-  git clone git://github.com/isaacs/nave.git ~/.nave
-fi
-
-if ! $(type node > /dev/null); then
-  echo 'now run "sudo ~/.nave/nave.sh usemain stable"'
-fi
