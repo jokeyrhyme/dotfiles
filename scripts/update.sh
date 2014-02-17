45ced89a7e3ea6972704eae6233ac59b0db1ae64
Ron Waldon <jokeyrhyme@gmail.com>
2014-02-17 16:50:07 +1100

    stop using boto

--- scripts/update.sh
+++ scripts/update.sh
@@ -36,10 +36,8 @@ fi
 if type ansible &> /dev/null; then
   if type pip2 &> /dev/null; then
     sudo pip2 install --upgrade ansible
-    sudo pip2 install --upgrade boto
   elif type pip &> /dev/null; then
     sudo pip install --upgrade ansible
-    sudo pip install --upgrade boto
   fi
 fi
 
