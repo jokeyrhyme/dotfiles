b3bf9444c314dac6c3bb27d053890e959f8d9d4f
Ron Waldon <jokeyrhyme@gmail.com>
2014-12-17 11:13:46 +1100

    update.sh: remove Haskell hack from `brew upgrade`

--- scripts/update.sh
+++ scripts/update.sh
@@ -12,7 +12,7 @@ fi
 
 if type brew > /dev/null 2>&1; then
   echo 'updating Homebrew and packages...'
-  brew update && DYLD_LIBRARY_PATH=/opt/homebrew/lib brew upgrade && brew linkapps && brew cleanup
+  brew update && brew upgrade && brew linkapps && brew cleanup
 fi
 
 if type brew-cask.rb > /dev/null 2>&1; then
