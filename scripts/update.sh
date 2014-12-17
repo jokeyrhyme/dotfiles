591fc4974a251acc37a2db10bdaa8e684d387c52
Ron Waldon <jokeyrhyme@gmail.com>
2014-12-17 11:13:14 +1100

    update.sh: fix stderr redirect to be POSIX-compliant

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
-if type yum &> /dev/null; then
+if type yum > /dev/null 2>&1; then
   echo 'updating packages with yum...'
   sudo yum upgrade -y
 fi
 
-if type pacman &> /dev/null; then
+if type pacman > /dev/null 2>&1; then
   echo 'updating packages with pacman...'
   sudo pacman -Syu
 fi
 
-if type brew &> /dev/null; then
+if type brew > /dev/null 2>&1; then
   echo 'updating Homebrew and packages...'
   brew update && DYLD_LIBRARY_PATH=/opt/homebrew/lib brew upgrade && brew linkapps && brew cleanup
 fi
 
-if type brew-cask.rb &> /dev/null; then
+if type brew-cask.rb > /dev/null 2>&1; then
   echo 'updating Homebrew Cask and packages...'
   brew cask cleanup
 fi
@@ -25,13 +25,13 @@ if type npm &> /dev/null; then
   npm -g update
 fi
 
-if type gem &> /dev/null; then
+if type gem > /dev/null 2>&1; then
   echo 'updating Ruby gems...'
   gem update
   gem cleanup
 fi
 
-if type apm &> /dev/null; then
+if type apm > /dev/null 2>&1; then
   echo 'updating Atom packages...'
   apm upgrade --confirm false
 fi
