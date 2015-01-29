ff418f6f49acaf351d61cedc4087c1a76c02ed67
Ron Waldon <jokeyrhyme@gmail.com>
2015-01-29 16:08:08 +1100

    scripts: add install-git.sh

---
+++ scripts/install-git.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+set -e
+
+if type brew > /dev/null 2>&1; then
+  echo 'found brew! preferring brew-installed git'
+  brew install git
+  exit 0
+fi
+
+if type git > /dev/null 2>&1; then
+  echo 'found git! no install necessary'
+  exit 0
+fi
+
+if type pacman > /dev/null 2>&1; then
+  echo 'found pacman!'
+  sudo pacman -Syu git
+  exit 0
+fi
+
+if type yum > /dev/null 2>&1; then
+  echo 'found yum!'
+  sudo yum install -y git
+  exit 0
+fi
+
+if type apt > /dev/null 2>&1; then
+  echo 'found apt!'
+  apt-get install -y git
+  exit 0
+fi
