5d3c5f50cd00f3cf3658e736f57f982d32182493
Ron Waldon <jokeyrhyme@gmail.com>
2015-01-29 16:13:00 +1100

    scripts: improve install-gitflow.sh to prefer brew

--- scripts/install-gitflow.sh
+++ scripts/install-gitflow.sh
@@ -1,2 +1,23 @@
 #!/bin/sh
+
+set -e
+
+if type git-flow > /dev/null 2>&1; then
+  echo 'found git-flow! no install necessary'
+  exit 0
+fi
+
+if type brew > /dev/null 2>&1; then
+  echo 'found brew!'
+  brew install git-flow
+  exit 0
+fi
+
+if type wget > /dev/null 2>&1; then
+  echo "found wget"
+else
+  echo "no wget!"
+  exit 1
+fi
+
 wget -q -O - https://raw.github.com/nvie/gitflow/develop/contrib/gitflow-installer.sh | sudo bash
