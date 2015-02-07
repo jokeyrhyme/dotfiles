11952b4bf8d5edfdf87c3d5e9a36a21c413909e9
Ron Waldon <jokeyrhyme@gmail.com>
2015-02-08 10:44:45 +1100

    packages: fonts: add Android fonts and a few others

--- packages/fonts-install.sh
+++ packages/fonts-install.sh
@@ -5,4 +5,5 @@ set -e
 if type pacman > /dev/null 2>&1; then
   echo 'found pacman!'
   sudo pacman -Sy --needed --noconfirm adobe-source-{code,sans,serif}-pro-fonts
+  sudo pacman -Sy --needed --noconfirm ttf-{droid,liberation,ubuntu-font-family}
 fi
