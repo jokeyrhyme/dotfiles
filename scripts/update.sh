060714f1e5d625a97d3a09b6b02e06e57bb80b27
Ron Waldon <jokeyrhyme@gmail.com>
2015-03-25 11:17:42 +1100

    update: `npm -g update` is safe with npm@2.6.1

--- scripts/update.sh
+++ scripts/update.sh
@@ -20,22 +20,12 @@ if type brew-cask.rb > /dev/null 2>&1; then
   brew cask cleanup
 fi
 
-# TODO: still not safe to do this
-# https://github.com/npm/npm/issues/6247
-#if type npm > /dev/null 2>&1; then
-#  echo 'updating NPM and packages...'
-#  npm -g update
-#fi
-
 source $(dirname $0)/../packages/nodejs-update.sh
 
 if type npm > /dev/null 2>&1; then
   echo 'updating NPM and packages...'
-  npm cache clean
-  for package in $(npm -g outdated --parseable --depth=0 | cut -d: -f2)
-  do
-    npm -g install "$package"
-  done
+  npm -g install npm
+  npm -g update
 fi
 
 source $(dirname $0)/../packages/ruby-update.sh
