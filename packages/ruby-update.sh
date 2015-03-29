1bc4795dda07d7e2e05da58f3220688893962b52
Ron Waldon <jokeyrhyme@gmail.com>
2015-03-30 06:45:25 +1100

    pkg: ruby: fix update condition, centralise version variable

--- packages/ruby-update.sh
+++ packages/ruby-update.sh
@@ -12,7 +12,7 @@ __dotfiles_update_shallow_git_clone ~/.rbenv/plugins/rbenv-vars
 __dotfiles_update_shallow_git_clone ~/.rbenv/plugins/rbenv-gem-rehash
 __dotfiles_update_shallow_git_clone ~/.rbenv/plugins/ruby-build
 
-if [ -d ~/.nvm/.git ]; then
+if [ -d ~/.rbenv/.git ]; then
 
   export PATH="$HOME/.rbenv/bin:$PATH"
   eval "$(rbenv init -)"
@@ -21,11 +21,11 @@ if [ -d ~/.nvm/.git ]; then
   if [ -x ~/.rbenv/versions/$RUBY_VERSION/bin/ruby ]; then
     echo "ruby $RUBY_VERSION is installed"
   else
-    TMPDIR=~/.rbenv/tmp rbenv install 2.2.0
+    TMPDIR=~/.rbenv/tmp rbenv install "$RUBY_VERSION"
     rm -fr ~/.rbenv/tmp/*
   fi
 
-  rbenv global 2.2.0
+  rbenv global "$RUBY_VERSION"
 
   # https://gist.github.com/mislav/5026283
   if [ -f $(ruby -ropenssl -e 'puts OpenSSL::X509::DEFAULT_CERT_FILE') ]; then
