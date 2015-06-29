9c3d6240c8ee57ebc55d45b5a776bcabda555e84
Ron Waldon <jokeyrhyme@gmail.com>
2015-06-29 21:55:43 +1000

    pkg: atom: use aurget to install atom-editor

--- packages/atom-install.sh
+++ packages/atom-install.sh
@@ -1,21 +1,11 @@
 #!/bin/sh
 
-if type pacman > /dev/null 2>&1; then
-  echo 'preparing ArchLinux for AUR atom-editor-bin...'
-  sudo pacman -S --needed abs
-  sudo mkdir -p /var/abs/local
-  sudo chmod -R g+w /var/abs/local
-  sudo chown -R root:wheel /var/abs/local
+if type aurget > /dev/null 2>&1; then
+  echo 'found aurget!'
+  aurget -Sy --noconfirm --noconfirm --builddir /var/abs/local atom-editor
+fi
 
-  echo 'installing AUR atom-editor-bin...'
-  URL=https://aur.archlinux.org/packages/at/atom-editor-bin/atom-editor-bin.tar.gz
-  DL=/var/abs/local/atom-editor-bin.tar.gz
-  rm -fr /var/abs/local/atom-editor-bin/
-  curl -o "$DL" "$URL"
-  pushd /var/abs/local
-  tar zxf "$DL"
-  pushd /var/abs/local/atom-editor-bin/
-  makepkg -s
-  sudo pacman -U *.pkg.tar
-  popd
+if [ -x /usr/bin/python2 ]; then
+  mkdir -p ~/.atom
+  echo "python=/usr/bin/python2" >> ~/.atom/.apmrc
 fi
