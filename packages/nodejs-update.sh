758472f5b2001522800379db3d4d08ae9916e826
Ron Waldon <jokeyrhyme@gmail.com>
2015-09-10 08:46:10 +1000

    pkg: node: use tagged nvm releases, add Node.js 4

--- packages/nodejs-update.sh
+++ packages/nodejs-update.sh
@@ -2,18 +2,23 @@
 
 # set -e # nvm.sh triggers this
 
-source $(dirname $0)/../scripts/lib/utils.sh
+. $(dirname $0)/../scripts/lib/utils.sh
 
 __dotfiles_assert_in_path git
 
-__dotfiles_update_shallow_git_clone ~/.nvm
+if [ -f ~/.nvm/install.sh ]; then
+  . ~/.nvm/install.sh
+fi
 
 if [ -d ~/.nvm/.git ]; then
-  source ~/.nvm/nvm.sh
+  . ~/.nvm/nvm.sh
+
+  nvm install 4.0
 
   nvm install 0.12
+
   nvm alias default 0.12
-  nvm use 0.12
+  nvm use default
 fi
 
 if [ -x /usr/bin/python2 ]; then
