1411c3fac3f9a914b017db3e53047076ce36856a
Ron Waldon <jokeyrhyme@gmail.com>
2016-04-27 09:41:31 +1000

    pkg: node: don't mkdir ~/.npm-packages

--- packages/nodejs-install.sh
+++ packages/nodejs-install.sh
@@ -8,8 +8,6 @@ __dotfiles_assert_in_path git
 
 __dotfiles_ensure_shallow_git_clone ~/.nvm https://github.com/creationix/nvm.git
 
-__dotfiles_force_mkdir ~/.npm-packages
-
 source $(dirname $0)/nodejs-update.sh
 
 npm install -g cordova
