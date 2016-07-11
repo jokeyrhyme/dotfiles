9b7f76a57373e5ecda0d0c4a7a80c1c77df70a80
Ron Waldon <jokeyrhyme@gmail.com>
2016-07-11 10:59:18 +1000

    pkg: nodejs: install favourite NPM packages

--- packages/nodejs-install.sh
+++ packages/nodejs-install.sh
@@ -9,7 +9,3 @@ __dotfiles_assert_in_path git
 __dotfiles_ensure_shallow_git_clone ~/.nvm https://github.com/creationix/nvm.git
 
 source $(dirname $0)/nodejs-update.sh
-
-npm install -g cordova
-npm install -g git-guilt
-npm install -g http-server
--- packages/nodejs-update.sh
+++ packages/nodejs-update.sh
@@ -34,7 +34,27 @@ npm config set save-exact true
 #   npm -g update
 # fi
 
+NPM_FAVOURITES=(
+  "cordova"
+  "git-guilt"
+  "grunt-cli"
+  "gulp"
+  "http-server"
+  "ionic"
+)
+
 if type npm > /dev/null 2>&1; then
+  echo 'installing favourite NPM packages...'
+  for NPM_FAVOURITE in ${NPM_FAVOURITES[@]}
+  do
+    if npm ls -g --depth=0 ${NPM_FAVOURITE} > /dev/null 2>&1; then
+      echo "- ${NPM_FAVOURITE} is already installed"
+    else
+      echo "- installing ${NPM_FAVOURITE}..."
+      npm install -g ${NPM_FAVOURITE}
+    fi
+  done
+
   echo 'updating NPM and packages...'
   npm cache clean
   for package in $(npm -g outdated --parseable --depth=0 | cut -d: -f2)
