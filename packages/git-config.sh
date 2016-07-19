1654212ace75d98670f17abdb5f05804ca361721
Ron Waldon <jokeyrhyme@gmail.com>
2016-07-19 15:19:08 +1000

    pkg: git: ensure desirable configuration

--- packages/git-config.sh
+++ packages/git-config.sh
@@ -3,9 +3,19 @@
 if type git > /dev/null 2>&1; then
   echo "configuring git..."
 
-  git config --global user.name "Ron Waldon"
   git config --global user.email "jokeyrhyme@gmail.com"
+  git config --global user.name "Ron Waldon"
 
-  git config --global push.default simple
+  # https://keybase.io/jokeyrhyme
+  git config --global user.signingKey "F96EC3B1"
 
+  git config --global push.default "simple"
+
+  if type gpg2 > /dev/null 2>&1; then
+    git config --global gpg.program "gpg2"
+  else
+    git config --global --unset gpg.program
+  fi
+
+  git config --global color.ui "auto"
 fi
