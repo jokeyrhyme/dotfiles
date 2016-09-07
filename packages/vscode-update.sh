f7fface9f1f59999a79702e7c85f661ac76b2bd8
Ron Waldon <jokeyrhyme@gmail.com>
2016-09-08 06:34:53 +1000

    pkg: vscode: assume extension support

--- packages/vscode-update.sh
+++ packages/vscode-update.sh
@@ -37,18 +37,11 @@ for VARIANT in "code-insiders" \
                "code"
 do
   if hash $VARIANT 2>/dev/null; then
-    VERSION="$($VARIANT --version)"
-    MAJOR="$(echo $VERSION | sed -e 's/\..*$//')"
-    MINOR="$(echo $VERSION | sed -e 's/\-[a-z]*$//' -e 's/^[0-9]*\.//' -e 's/\.[0-9]*$//')"
-    if [ "$MAJOR" -gt "0" ] && [ "$MINOR" -gt "1" ]; then
-      echo "Installing extensions for $VARIANT"
-      for EXTENSION in ${EXTENSIONS[@]}
-      do
-        $VARIANT --install-extension $EXTENSION
-      done
-    else
-      echo "$VARIANT (v$VERSION) does not support extension CLI args"
-    fi
+    echo "Installing extensions for $VARIANT"
+    for EXTENSION in ${EXTENSIONS[@]}
+    do
+      $VARIANT --install-extension $EXTENSION
+    done
   fi
 done
 
