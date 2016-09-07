079ba09d6d75499e86aba38cee29c058625ed391
Ron Waldon <jokeyrhyme@gmail.com>
2016-09-08 06:50:58 +1000

    pkg: atom: use the -bin package

--- packages/atom-install.sh
+++ packages/atom-install.sh
@@ -2,7 +2,7 @@
 
 if type aurget > /dev/null 2>&1; then
   echo 'found aurget!'
-  aurget -Sy --noconfirm --noconfirm --builddir /var/abs/local atom-editor
+  aurget -Sy --noconfirm --noconfirm --builddir /var/abs/local atom-editor-bin
 fi
 
 if [ -x /usr/bin/python2 ]; then
