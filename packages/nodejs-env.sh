55a000e4a6bf531a7df621530dbb1f0a699c1f32
Ron Waldon <jokeyrhyme@gmail.com>
2016-11-24 11:09:28 +1100

    pkg: nodejs: fix PATH for `yarn global add`

--- packages/nodejs-env.sh
+++ packages/nodejs-env.sh
@@ -16,3 +16,7 @@
 if [ -d ~/.yarn/bin ]; then
   export PATH="$HOME/.yarn/bin:$PATH"
 fi
+
+if [ -d ~/.config/yarn/global/node_modules/.bin ]; then
+  export PATH="$HOME/.config/yarn/global/node_modules/.bin:$PATH"
+fi
