2e396d1676f737647f7996dd47247c3786e4794b
Ron Waldon <jokeyrhyme@gmail.com>
2017-03-22 05:40:40 +1100

    pkg: golang: disable glide installer (temp)

--- packages/golang-update.sh
+++ packages/golang-update.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-if [ -d ~/go/bin ]; then
-  echo "installing latest glide.sh ..."
-  curl https://raw.githubusercontent.com/Masterminds/glide.sh/master/get | sh
-fi
+# if [ -d ~/go/bin ]; then
+#   echo "installing latest glide.sh ..."
+#   curl https://raw.githubusercontent.com/Masterminds/glide.sh/master/get | sh
+# fi
