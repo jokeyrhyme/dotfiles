7b80c6f2dd29358d442087d6d7ae0cc3f8b2ba1c
Ron Waldon <jokeyrhyme@gmail.com>
2017-04-19 21:29:54 +1000

    pkg: zsh: install pure as per instructions

--- packages/zsh-update.sh
+++ packages/zsh-update.sh
@@ -10,12 +10,15 @@ __dotfiles_assert_in_path git
 
 ## just let oh-my-zsh use its own updater for now
 
+# delete old installation of pure
+rm ~/.oh-my-zsh/custom/themes/pure.zsh-theme
+rm ~/.oh-my-zsh/functions/async
+
+# install pure theme
 __dotfiles_ensure_shallow_git_clone ~/.zsh-pure https://github.com/sindresorhus/pure.git
 
-mkdir -p ~/.oh-my-zsh/custom/themes
-cp -v ~/.zsh-pure/pure.zsh ~/.oh-my-zsh/custom/themes/pure.zsh-theme
-
-mkdir -p ~/.oh-my-zsh/functions
-cp -v ~/.zsh-pure/async.zsh ~/.oh-my-zsh/functions/async
+mkdir -p ~/.oh-my-zsh/custom
+cp -v ~/.zsh-pure/pure.zsh ~/.oh-my-zsh/custom/pure.zsh-theme
+cp -v ~/.zsh-pure/async.zsh ~/.oh-my-zsh/custom/async.zsh
 
 rm -rf ~/.zsh-pure
