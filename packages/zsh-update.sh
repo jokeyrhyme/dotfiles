44fa6281159de9d4817e031e3642ae128115a0ac
Ron Waldon <jokeyrhyme@gmail.com>
2017-04-19 21:37:36 +1000

    pkg: zsh: fix update script

--- packages/zsh-update.sh
+++ packages/zsh-update.sh
@@ -11,8 +11,8 @@ __dotfiles_assert_in_path git
 ## just let oh-my-zsh use its own updater for now
 
 # delete old installation of pure
-rm ~/.oh-my-zsh/custom/themes/pure.zsh-theme
-rm ~/.oh-my-zsh/functions/async
+rm -f ~/.oh-my-zsh/custom/themes/pure.zsh-theme
+rm -f ~/.oh-my-zsh/functions/async
 
 # install pure theme
 __dotfiles_ensure_shallow_git_clone ~/.zsh-pure https://github.com/sindresorhus/pure.git
