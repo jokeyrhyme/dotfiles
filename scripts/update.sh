f886224dec0a16fcd3c91943eda684b8a239ec06
Ron Waldon <jokeyrhyme@gmail.com>
2017-05-11 20:33:50 +1000

    scripts: reorder sequence in update.sh

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-pushd "$(dirname $0)/.." > /dev/null
-# scripts that break because of `set -e` # TODO: fixme!
-. ./packages/nodejs-update.sh
-popd > /dev/null
-
 if which dnf > /dev/null 2>&1; then
   echo 'updating packages with dnf...'
   sudo dnf upgrade --obsoletes --allowerasing
@@ -16,22 +11,25 @@ fi
 if which pacman > /dev/null 2>&1; then
   echo 'updating packages with pacman...'
   sudo pacman -Syu
-fi
 
-if which aurget > /dev/null 2>&1; then
-  echo 'updating packages with aurget...'
-  aurget -Syu --noedit
+  if which aurget > /dev/null 2>&1; then
+    echo 'updating packages with aurget...'
+    aurget -Syu --noedit
+  fi
 fi
 
 pushd "$(dirname $0)/.." > /dev/null
-# scripts that are fine with `set -e`
-. ./packages/git-config.sh
+. ./packages/homebrew-update.sh
+
 . ./packages/bash-update.sh
 . ./packages/zsh-update.sh
-. ./packages/homebrew-update.sh
+
 . ./packages/golang-update.sh
+. ./packages/nodejs-update.sh
 . ./packages/ruby-update.sh
+
 . ./packages/atom-update.sh
+. ./packages/git-config.sh
 . ./packages/vagrant-update.sh
 . ./packages/vim-update.sh
 . ./packages/vscode-update.sh
