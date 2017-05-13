636186509c9fcd153167cb453f14d88445c66d45
Ron Waldon <jokeyrhyme@gmail.com>
2017-05-13 17:56:22 +1000

    pkg: brew: fix LinuxBrew env

--- packages/linuxbrew-env.sh
+++ packages/linuxbrew-env.sh
@@ -4,4 +4,5 @@ if [ -d "${HOME}/.linuxbrew/bin" ]; then
   export PATH="${HOME}/.linuxbrew/bin:${PATH}"
   export INFOPATH="/home/ron/.linuxbrew/share/info:$INFOPATH"
   export MANPATH="/home/ron/.linuxbrew/share/man:$MANPATH"
+  export XDG_DATA_DIRS="/home/ron/.linuxbrew/share:$XDG_DATA_DIRS"
 fi
