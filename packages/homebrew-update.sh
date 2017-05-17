fcd01017706692448ce3a257940914aee2f131d7
Ron Waldon <jokeyrhyme@gmail.com>
2017-05-17 20:47:13 +1000

    pkg: brew: update macOS casks (fixes #14)

--- packages/homebrew-update.sh
+++ packages/homebrew-update.sh
@@ -6,6 +6,7 @@ pushd "$(dirname $0)/.." > /dev/null
 . ./scripts/lib/is.sh
 popd > /dev/null
 
+# brew cannot update zsh if it is locked down
 if __dotfiles_is_homebrew_found; then
   sudo chmod g+w /usr/local/share/zsh /usr/local/share/zsh/site-functions
 fi
@@ -15,11 +16,18 @@ if __dotfiles_is_brew_found; then
   brew update && brew upgrade && brew cleanup
 fi
 
+# zsh likes to be locked down for normal use
 if __dotfiles_is_homebrew_found; then
   sudo chmod g-w /usr/local/share/zsh /usr/local/share/zsh/site-functions
 fi
 
-if which brew-cask.rb > /dev/null 2>&1; then
-  echo 'updating Homebrew Cask and packages...'
-  brew cask cleanup
+if __dotfiles_is_homebrew_found; then
+  if which brew-cask.rb > /dev/null 2>&1; then
+    echo 'updating Homebrew Cask and packages...'
+
+    for f in $(brew cask outdated | cut -f 1 -d ''); do
+      brew cask install --force "$f"
+    done
+    brew cask cleanup
+  fi
 fi
