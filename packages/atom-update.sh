67b77b4397b50a5d214461f749b1fad19cd233ef
Ron Waldon <jokeyrhyme@gmail.com>
2017-05-17 21:02:27 +1000

    pkg: atom: optimise update.sh like vscode

--- packages/atom-update.sh
+++ packages/atom-update.sh
@@ -1,10 +1,62 @@
 #!/bin/bash
 
+set -e
+
+EXTENSIONS=(
+  "Zen"
+  "atom-wrap-in-tag"
+  "autocomplete-modules"
+  "autocomplete-paths"
+  "editorconfig"
+  "emmet"
+  "file-icons"
+  "go-debug"
+  "go-plus"
+  "go-signature-statusbar"
+  "hyperclick"
+  "language-babel"
+  "language-docker"
+  "language-markdown"
+  "language-swift"
+  "language-terraform"
+  "linter"
+  "linter-alex"
+  "linter-csslint"
+  "linter-eslint"
+  "linter-htmlhint"
+  "linter-markdown"
+  "linter-php"
+  "linter-proselint"
+  "linter-shellcheck"
+  "linter-write-good"
+  "merge-conflicts"
+  "minimap"
+  "minimap-find-and-replace"
+  "minimap-git-diff"
+  "minimap-linter"
+  "minimap-selection"
+)
+
+UNINSTALL_EXTENSIONS=()
+
 if which apm > /dev/null 2>&1; then
-  if apm stars > /dev/null 2>&1; then
-    echo 'installing favourite Atom packages...'
-    apm stars --install
-  fi
+  INSTALLED_EXTENSIONS=$(apm ls --bare --intalled --packaged)
+
+  echo "Installing extensions for Atom..."
+  for EXTENSION in "${EXTENSIONS[@]}"
+  do
+    if ! echo "${INSTALLED_EXTENSIONS}" | grep "${EXTENSION}" > /dev/null 2>&1; then
+      apm install --quiet --production --check "$EXTENSION"
+    fi
+  done
+
+  echo "Uninstalling unused extensions for Atom..."
+  for EXTENSION in "${UNINSTALL_EXTENSIONS[@]}"
+  do
+    if echo "${INSTALLED_EXTENSION}" | grep " ${EXTENSION}@" > /dev/null 2>&1; then
+      apm uninstall --hard "$EXTENSION"
+    fi
+  done
 
   echo 'updating Atom packages...'
   apm upgrade --confirm false
