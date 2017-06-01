5908e07103cbc5af0dd775a3e60df6f981ef823f
Ron Waldon <jokeyrhyme@gmail.com>
2017-06-01 21:44:47 +1000

    pkg: nodejs: fix update.sh (fixes #20)

--- packages/nodejs-update.sh
+++ packages/nodejs-update.sh
@@ -11,7 +11,7 @@ __dotfiles_assert_in_path git
 
 __dotfiles_ensure_shallow_git_clone ~/.nvs https://github.com/jasongin/nvs.git
 
-if which nvs > /dev/null 2>&1; then
+if nvs --version > /dev/null 2>&1; then
   nvs add 4
   nvs add 6
   nvs add lts
@@ -19,51 +19,52 @@ if which nvs > /dev/null 2>&1; then
   nvs link latest
 fi
 
-if [ -x /usr/bin/python2 ]; then
-  npm config set python /usr/bin/python2
-fi
-
-npm config set cache-min 3600
-
-npm config set init.author.name 'Ron Waldon'
-npm config set init.author.email 'jokeyrhyme@gmail.com'
-npm config set init.author.url 'https://github.com/jokeyrhyme'
-
-npm config set save-exact true
-
-npm config set send-metrics true
-
-NPM_FAVOURITES=(
-  "npm"
-  "@jokeyrhyme/node-init"
-  "@angular/cli"
-  "cordova"
-  "create-react-app"
-  "create-react-native-app"
-  "diff-so-fancy"
-  "ember-cli"
-  "eslint"
-  "git-guilt"
-  "grunt-cli"
-  "gulp"
-  "flow-bin"
-  "flow-typed"
-  "http-server"
-  "ionic"
-  "lebab"
-  "lerna"
-  "package-diff-summary"
-  "prettier"
-  "react-native-cli"
-  "typings"
-  "unleash"
-)
-
-UNINSTALL_NPM_FAVOURITES=(
-  "greenkeeper"
-)
-
 if which npm > /dev/null 2>&1; then
+
+  if [ -x /usr/bin/python2 ]; then
+    npm config set python /usr/bin/python2
+  fi
+
+  npm config set cache-min 3600
+
+  npm config set init.author.name 'Ron Waldon'
+  npm config set init.author.email 'jokeyrhyme@gmail.com'
+  npm config set init.author.url 'https://github.com/jokeyrhyme'
+
+  npm config set save-exact true
+
+  npm config set send-metrics true
+
+  NPM_FAVOURITES=(
+    "npm"
+    "@jokeyrhyme/node-init"
+    "@angular/cli"
+    "cordova"
+    "create-react-app"
+    "create-react-native-app"
+    "diff-so-fancy"
+    "ember-cli"
+    "eslint"
+    "git-guilt"
+    "grunt-cli"
+    "gulp"
+    "flow-bin"
+    "flow-typed"
+    "http-server"
+    "ionic"
+    "lebab"
+    "lerna"
+    "package-diff-summary"
+    "prettier"
+    "react-native-cli"
+    "typings"
+    "unleash"
+  )
+
+  UNINSTALL_NPM_FAVOURITES=(
+    "greenkeeper"
+  )
+
   echo 'updating NPM and packages...'
   npm update --global
 
