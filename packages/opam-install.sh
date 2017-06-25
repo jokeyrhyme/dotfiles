2152e155a44336691681838fb4ac18b67298c8dc
Ron Waldon <jokeyrhyme@gmail.com>
2017-06-25 17:45:00 +1000

    pkg: opam: new package

---
+++ packages/opam-install.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+
+set -e
+
+pushd "$(dirname $0)/.." > /dev/null
+. ./scripts/lib/is.sh
+. ./scripts/lib/utils.sh
+popd > /dev/null
+
+__dotfiles_assert_in_path brew
+
+if __dotfiles_is_brew_found; then
+  echo "installing opam tools via Homebrew..."
+  brew install opam
+fi
