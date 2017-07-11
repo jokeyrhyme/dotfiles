63799d4c00377be7290e994cd7c0b3108b428069
Ron Waldon <jokeyrhyme@gmail.com>
2017-07-12 06:36:13 +1000

    pkg: golang: install/update delve (fixes #23)

--- packages/golang-update.sh
+++ packages/golang-update.sh
@@ -6,5 +6,8 @@ if which go > /dev/null 2>&1; then
   if [ -d ~/go/bin ]; then
     echo "installing latest glide.sh ..."
     go get -u github.com/Masterminds/glide
+
+    echo "installing latest delve ..."
+    go get -u github.com/derekparker/delve/cmd/dlv
   fi
 fi
