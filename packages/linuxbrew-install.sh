02ccfb51a4a4c7787c39f01961c09953d8e711c6
Ron Waldon <jokeyrhyme@gmail.com>
2017-07-14 21:11:49 +1000

    pkg: brew: better ubuntu compatibility

--- packages/linuxbrew-install.sh
+++ packages/linuxbrew-install.sh
@@ -10,6 +10,11 @@ __dotfiles_assert_in_path curl
 __dotfiles_assert_in_path git
 __dotfiles_assert_in_path ruby
 
+if which apt-get > /dev/null 2>&1; then
+  sudo apt-get install -y build-essential
+fi
+
+
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
 
 brew tap linuxbrew/xorg
