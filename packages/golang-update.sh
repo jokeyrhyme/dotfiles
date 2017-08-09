083a330f9b748ed7b545917bdd81a4ba686d0763
Ron Waldon <jokeyrhyme@gmail.com>
2017-08-09 20:49:59 +1000

    pkg: golang: ensure `dep` is installed

--- packages/golang-update.sh
+++ packages/golang-update.sh
@@ -9,5 +9,8 @@ if which go > /dev/null 2>&1; then
 
     echo "installing latest delve ..."
     go get -u github.com/derekparker/delve/cmd/dlv
+
+    echo "installing latest dep ..."
+    go get -u github.com/golang/dep/cmd/dep
   fi
 fi
