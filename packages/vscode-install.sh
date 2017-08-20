8e9814abc0c9576b7eab0428f963cd6e33d2f885
Ron Waldon <jokeyrhyme@gmail.com>
2017-08-20 14:24:39 +1000

    pkg: vscode: automate Fedora installation

--- packages/vscode-install.sh
+++ packages/vscode-install.sh
@@ -8,6 +8,17 @@ if which pacaur > /dev/null 2>&1; then
   pacaur -Sy --needed --noconfirm visual-studio-code
 fi
 
+# https://code.visualstudio.com/docs/setup/linux#_rhel-fedora-and-centos-based-distributions
+if which rpm > /dev/null 2>&1; then
+  sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+  sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+  
+  if which dnf > /dev/null 2>&1; then
+    dnf check-update
+    sudo dnf install code
+  fi
+fi
+
 # Linux
 if [ -d $HOME/.config/Code ]; then
   VSCODE_CFG_DIR=$HOME/.config/Code
