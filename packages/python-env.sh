8aaaecf567567d5130163dead890d7b36d5ea79b
Ronald Waldon <rwaldon@atlassian.com>
2018-07-03 09:29:22 -0700

    pkg: python: bump to 3.7; disable ruby update

--- packages/python-env.sh
+++ packages/python-env.sh
@@ -4,11 +4,11 @@ if [ -d "$HOME"/Library/Python/2.7/bin ]; then
   export PATH=$HOME/Library/Python/2.7/bin:$PATH
 fi
 
-if [ -d "$HOME"/Library/Python/3.6/bin ]; then
-  export PATH=$HOME/Library/Python/3.6/bin:$PATH
+if [ -d "$HOME"/Library/Python/3.7/bin ]; then
+  export PATH=$HOME/Library/Python/3.7/bin:$PATH
 fi
 
-if [ -f "$HOME"/Library/Python/3.6/share/cloudtoken/shell_additions/bashrc_additions ]; then
+if [ -f "$HOME"/Library/Python/3.7/share/cloudtoken/shell_additions/bashrc_additions ]; then
   # shellcheck disable=SC1090
-  . "$HOME"/Library/Python/3.6/share/cloudtoken/shell_additions/bashrc_additions
+  . "$HOME"/Library/Python/3.7/share/cloudtoken/shell_additions/bashrc_additions
 fi
