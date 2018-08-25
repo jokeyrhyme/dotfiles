3ecaa152a695010e0c17f89651e846fa1140cb7d
Ron Waldon <jokeyrhyme@gmail.com>
2018-08-25 18:27:27 +0000

    pkg: local: add nvim to PATH

--- packages/local-env.sh
+++ packages/local-env.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
-export PATH="$HOME/.local/bin:$HOME/.local/go/bin:$HOME/.local/node/bin:$PATH"
+export PATH="$HOME/.local/bin:$HOME/.local/go/bin:$PATH"
+
+if [ -d "$HOME/.local/node/bin" ]; then
+  export PATH="$HOME/.local/node/bin:$PATH"
+fi
+
+if [ -d "$HOME/.local/nvim/bin" ]; then
+  export PATH="$HOME/.local/nvim/bin:$PATH"
+fi
+
