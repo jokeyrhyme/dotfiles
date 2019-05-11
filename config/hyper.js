5fcc0d0fed0d9052bd6b2c091883b01ed2462c93
Ron Waldon <jokeyrhyme@gmail.com>
2019-05-11 17:38:42 +1000

    cfg: hyper: rely more upon default, tweaks

--- config/hyper.js
+++ config/hyper.js
@@ -1,47 +1,21 @@
 module.exports = {
   config: {
-    // default font size in pixels for all tabs
-    fontSize: 14,
-
     // font family with optional fallbacks
-    fontFamily: '"Fira Mono", Hack, Menlo, "DejaVu Sans Mono", "Lucida Console", monospace',
-
-    // set to true for blinking cursor
-    cursorBlink: true,
+    fontFamily: '"Fira Code", "Fira Mono", monospace',
 
     // set to `true` if you're using a Linux set up
     // that doesn't shows native menus
     // default: `false` on Linux, `true` on Windows (ignored on macOS)
     showHamburgerMenu: true,
 
-    // set to `false` if you want to hide the minimize, maximize and close buttons
-    // additionally, set to `'left'` if you want them on the left, like in Ubuntu
-    // default: `true` on windows and Linux (ignored on macOS)
-    showWindowControls: true,
+    // custom padding (CSS format, i.e.: `top right bottom left`)
+    padding: '2px',
 
     // set to false for no bell
     bell: 'SOUND',
 
     // if true, selected text will automatically be copied to the clipboard
     copyOnSelect: true,
-
-    // for advanced config flags please refer to https://hyper.is/#cfg
-
-    hyperTransparentDynamic: {
-      alpha: 0.9
-    }
   },
-
-  // a list of plugins to fetch and install from npm
-  // format: [@org/]project[#version]
-  // examples:
-  //   `hyperpower`
-  //   `@company/project`
-  //   `project#1.0.1`
-  plugins: [],
-
-  // in development, you can create a directory under
-  // `~/.hyper_plugins/local/` and include it here
-  // to load it and avoid it being `npm install`ed
-  localPlugins: []
 };
+
