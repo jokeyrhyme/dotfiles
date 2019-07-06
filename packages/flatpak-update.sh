176d4401bcb36a81cbd9f7a7b9bfb833d6b47f21
Ron Waldon <jokeyrhyme@gmail.com>
2019-07-07 06:50:11 +1000

    pkg: flatpak: don't add GNOME remote

--- packages/flatpak-update.sh
+++ packages/flatpak-update.sh
@@ -3,10 +3,7 @@
 set -e
 
 if which flatpak >/dev/null 2>&1; then
-  flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-
-  flatpak remote-add --if-not-exists gnome https://sdk.gnome.org/gnome.flatpakrepo
-  flatpak remote-add --if-not-exists gnome-apps https://sdk.gnome.org/gnome-apps.flatpakrepo
+  flatpak remote-add --if-not-exists --user flathub https://flathub.org/repo/flathub.flatpakrepo
 
   flatpak update
 fi
