f99c317387edd26cd7fce84eeef522d1265999d2
Ron Waldon <jokeyrhyme@gmail.com>
2020-07-26 20:51:06 +1000

    cfg: hyper: theme and other tweaks

--- config/hyper.js
+++ config/hyper.js
@@ -2,7 +2,7 @@ module.exports = {
   config: {
     // font family with optional fallbacks
     fontFamily: '"Fira Code", "Cascadia Code", "Fira Mono", monospace',
-    fontSize: 14,
+    fontSize: 12,
 
     // set to `true` if you're using a Linux set up
     // that doesn't shows native menus
@@ -13,10 +13,11 @@ module.exports = {
     padding: '2px',
 
     // set to false for no bell
-    bell: 'SOUND',
+    bell: false,
 
     // if true, selected text will automatically be copied to the clipboard
     copyOnSelect: true,
   },
+  plugins: ['hyper-dracula'],
 };
 
