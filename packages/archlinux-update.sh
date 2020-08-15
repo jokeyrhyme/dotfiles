9d4b6fffa295f23debcf4cab4f6747e4d31a7fe5
Ron Waldon <jokeyrhyme@gmail.com>
2020-08-15 11:46:46 +1000

    pkg: archlinux: more favourites

--- packages/archlinux-update.sh
+++ packages/archlinux-update.sh
@@ -7,26 +7,24 @@ pushd "$(dirname $0)/.." >/dev/null
 popd >/dev/null
 
 if command -v pacman >/dev/null 2>&1; then
-  echo 'installing toolchain with pacman ...'
-  sudo pacman -S --needed --noconfirm base-devel cmake expac kcov lldb yajl
+  sudo pacman -Su --needed --noconfirm archlinux-keyring
 
-  #  echo 'installing / updating AUR pacaur ...'
-  #  __dotfiles_ensure_shallow_git_clone ~/.pacaur-repo https://github.com/rmarquis/pacaur.git
-  #  sudo cp -v ~/.pacaur-repo/pacaur /usr/local/bin/pacaur
-  #  sudo chmod a+rx /usr/local/bin/pacaur
-
-  echo 'updating packages with pacman ...'
-  sudo pacman -Su --needed archlinux-keyring
-  sudo pacman -Syu
-
+  # TODO: try to keep in sync with config/Brewfile
   echo 'installing favourites with pacman ...'
+  sudo pacman -S --needed --noconfirm base-devel cmake expac kcov lldb yajl
   sudo pacman -S --needed --noconfirm bash fish gnupg lynis neofetch tmux zsh
   sudo pacman -S --needed --noconfirm pandoc # needed for checkmake # TODO: haskell?
+  sudo pacman -S --needed --noconfirm opensc yubikey-manager
+  sudo pacman -S --needed --noconfirm alacritty keybase{,-gui} python-pipx neovim
+  sudo pacman -S --needed --noconfirm sway swaybg swayidle swaylock waybar wofi
   sudo pacman -S --needed --noconfirm adobe-source-{code,sans,serif}-pro-fonts noto-fonts{,-emoji} otf-overpass ttf-{cascadia-code,fira-{code,mono,sans},hack,jetbrains-mono,roboto{,-mono}} inter-font
-  # TODO: try to keep in sync with config/Brewfile
+  if pacman -Q keybase-bin > /dev/null 2>&1; then
+    sudo pacman -R --noconfirm --unneeded keybase-bin # from AUR, now in community
+  fi
+  if pacman -Q redshift > /dev/null 2>&1; then
+    sudo pacman -R --noconfirm --unneeded redshift # doesn't work on wayland
+  fi
 
-#  if command -v pacaur >/dev/null 2>&1; then
-#    echo 'updating packages with pacaur...'
-#    pacaur -Syu --aur
-#  fi
+  echo 'updating packages with pacman ...'
+  sudo pacman -Syu
 fi
