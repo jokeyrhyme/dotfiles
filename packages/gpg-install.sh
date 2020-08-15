e4f7e19494c7424ce80d40cf958b854bffc942a5
Ron Waldon <jokeyrhyme@gmail.com>
2020-08-15 11:24:37 +1000

    pkg: gpg: drop keybase steps in favour of distro

--- packages/gpg-install.sh
+++ packages/gpg-install.sh
@@ -8,19 +8,10 @@ popd >/dev/null
 
 if command -v dnf >/dev/null 2>&1; then
   sudo dnf install -y gnupg2
-  sudo dnf install -y https://prerelease.keybase.io/keybase_amd64.rpm
-fi
-
-if command -v pacaur >/dev/null 2>&1; then
-  sudo pacman -Sy --needed --noconfirm perl-xml-parser
 fi
 
 if __dotfiles_is_homebrew_found; then
   brew install gpg2
-fi
-
-if command -v pacaur >/dev/null 2>&1; then
-  pacaur -Sy --needed --noconfirm keybase-bin
 fi
 
 pushd "$(dirname $0)/.." >/dev/null
--- packages/gpg-update.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env sh
-
-set -e
-
-if command -v run_keybase >/dev/null 2>&1; then
-  run_keybase
-fi
