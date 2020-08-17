0903398d8bb630c3bd74e6ea87a8e4dedaaa8af8
Ron Waldon <jokeyrhyme@gmail.com>
2020-08-17 20:33:27 +1000

    cfg: sway: use `loginctl lock-session`

--- scripts/loginctl-lock-session.sh
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env sh
-
-# `loginctl lock-sessions` (plural) requires admin powers,
-# and `loginctl lock-session` does not have a user filter option
-
-for SESSION_ID in $(loginctl list-sessions --no-ask-password --output json | jq -r ".[] | select(.user==\"${USER}\") | .session"); do
-  loginctl lock-session "${SESSION_ID}"
-done
